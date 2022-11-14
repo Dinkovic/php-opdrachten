cb05fcdfd2c75bd0135e9e48b2ccfb8a8a59caf1
Dinkovic <118133028+Dinkovic@users.noreply.github.com>
2022-11-14 15:24:00 +0100

    Add files via upload

---
+++ php2.1.5.php
@@ -0,0 +1,21 @@
+<table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
+<?php
+for($row=1; $row<=8; $row++)
+{
+	echo "<tr>";
+	for($column=1; $column<=8; $column++)
+	{
+		$total=$row+$column;
+		if($total%2==0)
+		{
+			echo "<td height=30px width=30px bgcolor=white></td>";
+		}
+		else
+		{
+			echo "<td height=30px width=30px bgcolor=black></td>";
+		}
+	}
+	echo "</tr>";
+}
+?>
+</table>
